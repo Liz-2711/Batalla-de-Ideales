221fe5ff2c551706fc3687cac79a202cc8e8306f
Liz-11 <85639388+Liz-11@users.noreply.github.com>
2021-10-27 23:15:34 -0600

    Audio & Pantalla listos

--- LabProject/Game.cpp
+++ LabProject/Game.cpp
@@ -1,6 +1,8 @@
 #include "Game.h"
 #include"SDL_image.h"
 
+
+
 SDL_Texture* playText;
 SDL_Rect scrR, destR;
 SDL_Rect    rcSprite;
@@ -74,6 +76,25 @@ void Game::init(const char* title, int WIDTH, int HEIGHT, bool fullscreen)
         gdSprite.w = 800;
         gdSprite.h = 800;
 
+
+//*****************************************************************************************************************************************************************************
+        SDL_AudioSpec wavSpec;
+        Uint32 wavLength;
+        Uint8* wavBuffer;
+
+        SDL_LoadWAV("IntroUT.wav", &wavSpec, &wavBuffer, &wavLength);
+
+        SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+
+        int success = SDL_QueueAudio(deviceId, wavBuffer, wavLength);
+        SDL_PauseAudioDevice(deviceId, 0);
+
+
+        // keep application running long enough to hear the sound
+
+        SDL_Delay(3000);
+       //************************************************************************************************************************************************
+
         while (!gameover)
         {
             if (SDL_PollEvent(&event))
@@ -103,24 +124,16 @@ void Game::init(const char* title, int WIDTH, int HEIGHT, bool fullscreen)
 
             if (keystate[SDL_SCANCODE_UP]) {
                // gdSprite.y -= 1;              
-                rcSprite.y += 2;
+                rcSprite.y += 1;
             }
             if (keystate[SDL_SCANCODE_DOWN]) {
              //   gdSprite.y += 1;
-                rcSprite.y -= 2;
+                rcSprite.y -= 1;
             }
             if (gdSprite.x < 0) {
                 gdSprite.x = 0;
             }
-          //  else if (gdSprite.x > WIDTH - SPRITE_SIZE) {
-             //   gdSprite.x = WIDTH - SPRITE_SIZE;
-           // }
-            /* if (gdSprite.y < 0) {
-                 gdSprite.y = 0;
-             }
-             else if (gdSprite.y > HEIGHT - SPRITE_SIZE) {
-                 gdSprite.y = HEIGHT - SPRITE_SIZE;
-             }*/
+          
 
              //Render the window
             SDL_RenderClear(renderer);
@@ -132,7 +145,13 @@ void Game::init(const char* title, int WIDTH, int HEIGHT, bool fullscreen)
 
 
         }
+      
 
+        // clean up
+
+        SDL_CloseAudioDevice(deviceId);
+        SDL_FreeWAV(wavBuffer);
+        
 
         SDL_DestroyTexture(spriteTexture);
         SDL_DestroyTexture(texture);
@@ -146,305 +165,8 @@ void Game::init(const char* title, int WIDTH, int HEIGHT, bool fullscreen)
         
     
     }
-	/*SDL_Event event;
-	SDL_Renderer* renderer = NULL;
-	SDL_Texture* texture = NULL;
-	SDL_Window* window = NULL;
-
-	SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO);
-	SDL_CreateWindowAndRenderer(
-		800, 500,
-		0, &window, &renderer
-	);
-	IMG_Init(IMG_INIT_PNG);
-	texture = IMG_LoadTexture(renderer, "DarkBK.png");
-	while (1) {
-		SDL_RenderCopy(renderer, texture, NULL, NULL);
-		SDL_RenderPresent(renderer);
-		if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-			break;
-	}
-
-	SDL_DestroyTexture(texture);
-	IMG_Quit();
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-	
-*/
-
-/*****************************************************************************************************************************************************
-    SDL_Surface* imageSurface = NULL;
-    SDL_Surface* windowSurface = NULL;
-    SDL_Surface* temp = NULL;
-    SDL_Surface* sprite = NULL;
-    SDL_Surface* SDL_DisplayFormat(SDL_Surface * surface);
-    SDL_Rect    rcSprite;
-    SDL_Rect    gdSprite;
-    SDL_Event windowEvent;
-    SDL_Event   event;
-    SDL_Renderer* renderer = NULL;
-    SDL_Texture* texture;
-    SDL_Texture* spriteTexture;
-
-    const Uint8* keystate;
-    int colorkey;
-    int count;
-    int xPosition = 0;
-    int yPosition = 0;
-    int gameover = 0;
-
-
-    SDL_Window* window = SDL_CreateWindow("ABDUCTO", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_ALLOW_HIGHDPI);
-    windowSurface = SDL_GetWindowSurface(window);
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-
-    imageSurface = IMG_Load("farm.png");
-    sprite = IMG_Load("DarkBK.png");
-
-    texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-    spriteTexture = SDL_CreateTextureFromSurface(renderer, sprite);
-
-    SDL_FreeSurface(sprite);
-    SDL_FreeSurface(imageSurface);
-
-    //rcSprite used as source rectangle, gdSprite as destination rectangle. Initialize them to the same position
-    rcSprite.x = xPosition;
-    rcSprite.y = yPosition;
-    rcSprite.w = SPRITE_SIZE;
-    rcSprite.h = SPRITE_SIZE;
-
-    gdSprite.x = xPosition;
-    gdSprite.y = yPosition;
-    gdSprite.w = SPRITE_SIZE;
-    gdSprite.h = SPRITE_SIZE;
-
-    while (!gameover)
-    {
-        if (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-            case SDL_QUIT:
-                gameover = 1;
-                break;
-
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.sym)
-                {
-                case SDLK_ESCAPE:
-                case SDLK_q:
-                    gameover = 1;
-                    break;
-
-                }
-                break;
-            }
-        }
-
-        keystate = SDL_GetKeyboardState(NULL);
-
-        // When key pressed, update the destination rectangle
-        if (keystate[SDL_SCANCODE_LEFT]) {
-            gdSprite.x -= 2;
-        }
-        if (keystate[SDL_SCANCODE_RIGHT]) {
-            gdSprite.x += 2;
-        }
-        if (keystate[SDL_SCANCODE_UP]) {
-            gdSprite.y -= 2;
-        }
-        if (keystate[SDL_SCANCODE_DOWN]) {
-            gdSprite.y += 2;
-        }
-        if (gdSprite.x < 0) {
-            gdSprite.x = 0;
-        }
-        else if (gdSprite.x > width - SPRITE_SIZE) {
-            gdSprite.x = width - SPRITE_SIZE;
-        }
-        if (gdSprite.y < 0) {
-            gdSprite.y = 0;
-        }
-        else if (gdSprite.y > height - SPRITE_SIZE) {
-            gdSprite.y = height - SPRITE_SIZE;
-        }
-
-        //Render the window
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderCopy(renderer, spriteTexture, &rcSprite, &gdSprite);
-        SDL_RenderPresent(renderer);
-
-        //SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
-        //SDL_BlitSurface(sprite, NULL, imageSurface, &rcSprite);
-        //SDL_UpdateWindowSurface(window);
-
-
-        //update the source rectangle to move with the sprite??
-        rcSprite.x = gdSprite.x;
-        rcSprite.y = gdSprite.y;
-
-    }
-
-
-    SDL_DestroyTexture(spriteTexture);
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-
 
    
-
-    SDL_Quit();
-
-    */
-        //**************************************************************************************************************************
-
-/*	int flags = 0;
-
-	if (fullscreen)
-	{
-		flags = SDL_WINDOW_FULLSCREEN;
-	}
-
-	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
-	{
-		window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
-		renderer = SDL_CreateRenderer(window, -1, 0);
-		if (renderer)
-		{
-			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-		}
-
-		isRunning = true;
-	}
-
-	SDL_Surface* tempSurface = IMG_Load("DarkBK.png");
-	
-	playText = SDL_CreateTextureFromSurface(renderer, tempSurface);
-
-	
-	SDL_FreeSurface(tempSurface);
-
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Surface* imageSurface = NULL;
-    SDL_Surface* windowSurface = NULL;
-    SDL_Surface* temp = NULL;
-    SDL_Surface* sprite = NULL;
-    SDL_Surface* SDL_DisplayFormat(SDL_Surface * surface);
-    SDL_Rect    rcSprite;
-    SDL_Rect    gdSprite;
-    SDL_Event windowEvent;
-    SDL_Event   event;
-    SDL_Renderer* renderer = NULL;
-    SDL_Texture* texture;
-    SDL_Texture* spriteTexture;
-
-    const Uint8* keystate;
-    int colorkey;
-    int count;
-    int xPosition = 400;
-    int yPosition = 0;
-    int gameover = 0;
-
-
-    SDL_Window* window = SDL_CreateWindow("Intro", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-    windowSurface = SDL_GetWindowSurface(window);
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-
-    //imageSurface = IMG_Load("IntroBK.png");
-    sprite = IMG_Load("IntroBK.png");
-
-    texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-    spriteTexture = SDL_CreateTextureFromSurface(renderer, sprite);
-
-    SDL_FreeSurface(sprite);
-    SDL_FreeSurface(imageSurface);
-
-    //rcSprite used as source rectangle, gdSprite as destination rectangle. Initialize them to the same position
-    rcSprite.x = 0;
-    rcSprite.y = 0;
-    rcSprite.w = SPRITE_SIZE;
-    rcSprite.h = SPRITE_SIZE;
-
-    gdSprite.x = xPosition;
-    gdSprite.y = yPosition;
-    gdSprite.w = SPRITE_SIZE;
-    gdSprite.h = SPRITE_SIZE;
-
-    while (!gameover)
-    {
-        if (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-            case SDL_QUIT:
-                gameover = 1;
-                break;
-
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.sym)
-                {
-                case SDLK_ESCAPE:
-                case SDLK_q:
-                    gameover = 1;
-                    break;
-
-                }
-                break;
-            }
-        }
-
-        keystate = SDL_GetKeyboardState(NULL);
-
-        // When key pressed, update the destination rectangle
-
-        if (keystate[SDL_SCANCODE_UP]) {
-            gdSprite.y -= 1;
-        }
-        if (keystate[SDL_SCANCODE_DOWN]) {
-            gdSprite.y += 1;
-        }
-        if (gdSprite.x < 0) {
-            gdSprite.x = 0;
-        }
-        else if (gdSprite.x > WIDTH - SPRITE_SIZE) {
-            gdSprite.x = WIDTH - SPRITE_SIZE;
-        }
-        if (gdSprite.y < 0) {
-            gdSprite.y = 0;
-        }
-        else if (gdSprite.y > HEIGHT - SPRITE_SIZE) {
-            gdSprite.y = HEIGHT - SPRITE_SIZE;
-        }
-
-        //Render the window
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, &gdSprite);
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderCopy(renderer, spriteTexture, &rcSprite, &gdSprite);
-        SDL_RenderPresent(renderer);
-
-
-
-    }
-
-
-    SDL_DestroyTexture(spriteTexture);
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-
-
-
-
-    SDL_Quit();
-    return 0;*/
 
 
 void Game::handleEvents()
@@ -491,6 +213,9 @@ void Game::render()
 
 void Game::clean()
 {
+    SDL_CloseAudio();
+    
+
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
 	SDL_Quit();
--- LabProject/Main.cpp
+++ LabProject/Main.cpp
@@ -1,18 +1,20 @@
 #include<SDL.h>
 #include <stdlib.h>
 #include"SDL_image.h"
+#include <SDL_mixer.h>
+
+
 
 
 #include "Game.h"
-const int WIDTH = 800;
-const int HEIGHT = 460;
-//const int SPRITE_SIZE = 100;
+
 
 Game* game = nullptr;
 
+
 int main(int argc, char* argv[])
 {
-
+   
 
      const int FPS = 60;
     const int frameDelay = 100 / FPS;
@@ -22,6 +24,8 @@ int main(int argc, char* argv[])
 
     game = new Game();
     game->init("GameWindow", 800, 600, false);
+
+  
 
     while (game->running())
     {
@@ -39,6 +43,10 @@ int main(int argc, char* argv[])
         }
     }
 
+
+    
+
+
     game->clean();
     return 0;
 
@@ -47,273 +55,3 @@ int main(int argc, char* argv[])
 
 
 
-
-
-    /*SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Surface* imageSurface = NULL;
-    SDL_Surface* windowSurface = NULL;
-    SDL_Surface* temp = NULL;
-    SDL_Surface* sprite = NULL;
-    SDL_Surface* SDL_DisplayFormat(SDL_Surface * surface);
-    SDL_Rect    rcSprite;
-    SDL_Rect    gdSprite;
-    SDL_Event windowEvent;
-    SDL_Event   event;
-    SDL_Renderer* renderer = NULL;
-    SDL_Texture* texture;
-    SDL_Texture* spriteTexture;
-
-    const Uint8* keystate;
-    int colorkey;
-    int count;
-    int xPosition = 400;
-    int yPosition = 0;
-    int gameover = 0;
-
-
-    SDL_Window* window = SDL_CreateWindow("Intro", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-    windowSurface = SDL_GetWindowSurface(window);
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-
-
-   
-    //imageSurface = IMG_Load("IntroBK.png");
-   sprite = IMG_Load("IntroBK.png");
-
-
-   int SPRITE_SIZE = sprite->h*sprite->w;
-    texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-    spriteTexture = SDL_CreateTextureFromSurface(renderer, sprite);
-
-    SDL_FreeSurface(sprite);
-    SDL_FreeSurface(imageSurface);
-
-    //rcSprite used as source rectangle, gdSprite as destination rectangle. Initialize them to the same position
-    rcSprite.x = 0;
-    rcSprite.y = 0;
-    rcSprite.w = 800;
-    rcSprite.h = 400;
-
-    gdSprite.x = xPosition;
-    gdSprite.y = yPosition;
-    gdSprite.w = 800;
-    gdSprite.h = 400;
-
-    while (!gameover)
-    {
-        if (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-            case SDL_QUIT:
-                gameover = 1;
-                break;
-
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.sym)
-                {
-                case SDLK_ESCAPE:
-                case SDLK_q:
-                    gameover = 1;
-                    break;
-
-                }
-                break;
-            }
-        }
-
-        keystate = SDL_GetKeyboardState(NULL);
-
-        // When key pressed, update the destination rectangle
-
-        if (keystate[SDL_SCANCODE_UP]) {
-            gdSprite.y -= 1;
-        }
-        if (keystate[SDL_SCANCODE_DOWN]) {
-            gdSprite.y += 1;
-        }
-        if (gdSprite.x < 0) {
-            gdSprite.x = 0;
-        }
-        else if (gdSprite.x > WIDTH - SPRITE_SIZE) {
-            gdSprite.x = WIDTH - SPRITE_SIZE;
-        }
-       /* if (gdSprite.y < 0) {
-            gdSprite.y = 0;
-        }
-        else if (gdSprite.y > HEIGHT - SPRITE_SIZE) {
-            gdSprite.y = HEIGHT - SPRITE_SIZE;
-        }*/
-
-        //Render the window
-        /*
-        * 
-        * 
-        * SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, &gdSprite);
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderCopy(renderer, spriteTexture, &rcSprite, &gdSprite);
-        SDL_RenderPresent(renderer);
-
-
-
-    }
-
-
-    SDL_DestroyTexture(spriteTexture);
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-
-
-
-
-    SDL_Quit();
-    return 0;
-}
-        */
-        //LAS FRAMES NOS AYUDAN A CORRER LA PANTALLA MAS LENTO
-
-        //
-      /*  const int FPS = 60;
-        const int frameDelay = 100 / FPS;
-
-        Uint32 frameStart;
-        int frameTime;
-
-        game = new Game();
-        game->init("GameWindow", 800, 600, false);
-
-        while (game->running())
-        {
-
-            frameStart = SDL_GetTicks();
-            game->handleEvents();
-            game->update();
-            game->render();
-
-            frameTime = SDL_GetTicks() - frameStart;
-
-            //Sin esto no hay delay ******
-            if (frameDelay > frameStart) {
-                SDL_Delay(frameDelay - frameTime);
-            }
-        }
-
-        game->clean();
-        return 0;
-
-
-
-
-
-
-
-
-
-
-
-   /* SDL_Init(SDL_INIT_EVERYTHING);
-    //For loading PNG images
-    IMG_Init(IMG_INIT_PNG);
-    SDL_Window* window = SDL_CreateWindow("Getting Started", SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-    SDL_Event input;
-    bool quit = false;
-
-    SDL_Texture* texture = NULL;
-    SDL_Surface* temp = IMG_Load("DarkBK.png");
-
-    //Filling texture with the image using a surface
-    texture = SDL_CreateTextureFromSurface(renderer, temp);
-    
-    //Deleting the temporary surface
-    SDL_FreeSurface(temp);
-
-    SDL_Rect rect;
-    rect.x = 0; //Extreme left of the window
-    rect.y = 0; //Very bottom of the window
-    rect.w = 800; //100 pixels width
-    rect.h = 600; //100 pixels height
-    //'rect' defines the dimensions for the bottom-left of the window
-
-    while (!quit)
-    {
-        while (SDL_PollEvent(&input) > 0)
-        {
-            if (input.type == SDL_QUIT) quit = true;
-        }
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
-        //Copying the texture on to the window using renderer and rectangle
-        SDL_RenderCopy(renderer, texture, NULL, &rect);
-
-        SDL_RenderPresent(renderer);
-    }
-    */
-  /**************************************************************************************************************************/
-   /* const SDL_MessageBoxButtonData buttons[] = {
-        { // .flags, .buttonid, .text 
-            0, 0, "no" },
-        { SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 1, "yes" },
-        { SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, 2, "cancel" },
-    };
-    const SDL_MessageBoxColorScheme colorScheme = {
-        { // .colors (.r, .g, .b) *
-            // [SDL_MESSAGEBOX_COLOR_BACKGROUND
-            { 255,   0,   0 },
-            //[SDL_MESSAGEBOX_COLOR_TEXT] *
-            {   0, 255,   0 },
-            // [SDL_MESSAGEBOX_COLOR_BUTTON_BORDER] *
-            { 255, 255,   0 },
-            // [SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND] *
-            {   0,   0, 255 },
-            // [SDL_MESSAGEBOX_COLOR_BUTTON_SELECTED] *
-            { 255,   0, 255 }
-        }
-    };
-    const SDL_MessageBoxData messageboxdata = {
-        SDL_MESSAGEBOX_INFORMATION, // .flags *
-        NULL, // .window *
-        "example message box", // .title 
-        "select a button", // .message *
-        SDL_arraysize(buttons), // .numbuttons /
-        buttons, // .buttons /
-        &colorScheme // .colorScheme /
-    };
-    int buttonid;
-    if (SDL_ShowMessageBox(&messageboxdata, &buttonid) < 0) {
-        SDL_Log("error displaying message box");
-        return 1;
-    }
-    if (buttonid == -1) {
-        SDL_Log("no selection");
-    }
-    else {
-        SDL_Log("selection was %s", buttons[buttonid].text);
-    }
-    system("PAUSE");
-    */
-
-    /**************************************************************************************************************************/
-
-    //Deleting the texture
- /*   SDL_DestroyTexture(texture);
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-
-    //For quitting IMG systems
-    IMG_Quit();
-
-    SDL_Quit();
-
-    return 0;*
-    }*/
-    
-		
